f166ebedeef20b1da095827cd142200fdf0d0d3e
pierfrancescoscar <scarinci401@gmail.com>
2021-10-21 15:52:47 +0200

    add discounted price for users aged < 18

--- js/main.js
+++ js/main.js
@@ -3,8 +3,27 @@
 // 1. Chiedi quanti kilometri vuole percorrere
 
 const km = prompt ('Quanti kilometri vuoi percorrere?');
+console.log('Km percorsi: ' + km);
 
 // 2. Chiedi l'età del passeggero
 
-const age = prompt ("Qual'è l'età del passeggero?");
+const age = prompt ("Qual è l'età del passeggero?");
+console.log('Età passeggero: ' + age);
+
+// 3. Definisci il prezzo del biglietto in base ai km da percorrere
+
+const price = km * 0.21;
+console.log('Prezzo biglietto per km percorso: ' + price);
+
+// 4. Definisci prezzo finale del biglietto
+
+let finalPrice = price * km;
+console.log ('Prezzo finale del biglietto: ' + finalPrice);
+
+// 5. Applica sconto del 20% se l'utente è minorenne 
+
+if (age < 18)  {
+    discountPrice = finalPrice - (finalPrice * 20 / 100);
+    console.log('Prezzo scontato per minorenni: ' + discountPrice);
+}
 
