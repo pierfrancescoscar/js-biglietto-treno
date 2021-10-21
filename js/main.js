3740f19329f099b90f5553144e9f2ba9fe3dd853
pierfrancescoscar <scarinci401@gmail.com>
2021-10-21 14:29:55 +0200

    js main settings and first questions

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero. //
+
+// 1. Chiedi quanti kilometri vuole percorrere
+
+const km = prompt ('Quanti kilometri vuoi percorrere?');
+
+// 2. Chiedi l'età del passeggero
+
+const age = prompt ("Qual'è l'età del passeggero?");
+
